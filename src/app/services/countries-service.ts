e39ed816ff7c5cb8927430bdb3643f84ef33e7c3
Ghost3298 <eddy.lebbos@hotmail.com>
2025-11-14 22:24:57 +0200

    test-add-country

--- src/app/services/countries-service.ts
+++ src/app/services/countries-service.ts
@@ -50,7 +50,7 @@ export class CountriesService {
 
       console.log('Sending data to add-country function');
       const response = await firstValueFrom(
-        this.http.post<Country>(`${this.baseUrl}/add-country`, [countryData.country_name, countryData.country_image], {
+        this.http.post<Country>(`${this.baseUrl}/test-add-country`, [countryData.country_name, countryData.country_image], {
           headers: {
             'Content-Type': 'application/json'
           }
